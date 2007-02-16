565658aa9a10e37ad9a7d0e42142997feb812496
onnepoika <onnepoika@4cd3fd0c-ba27-0410-8e24-a7ae84797233>
2007-02-16 08:48:30 +0000

    UI

--- MusicBrainzDotNet-2.1.1/examples/WindowsApplication1/Form1.Designer.cs
+++ MusicBrainzDotNet-2.1.1/examples/WindowsApplication1/Form1.Designer.cs
@@ -1,4 +1,7 @@
-namespace WindowsApplication1
+using System;
+using System.IO;
+
+namespace MusicDataminer
 {
     partial class Form1
     {
@@ -28,12 +31,143 @@ namespace WindowsApplication1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.splitLayout = new System.Windows.Forms.SplitContainer();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.stylesLabel = new System.Windows.Forms.Label();
+            this.styleCheckBoxList = new System.Windows.Forms.CheckedListBox();
+            this.startQueryButton = new System.Windows.Forms.Button();
+            this.outputBox = new System.Windows.Forms.TextBox();
+            this.splitLayout.Panel1.SuspendLayout();
+            this.splitLayout.Panel2.SuspendLayout();
+            this.splitLayout.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitLayout
+            // 
+            this.splitLayout.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitLayout.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+            this.splitLayout.Location = new System.Drawing.Point(0, 0);
+            this.splitLayout.Name = "splitLayout";
+            // 
+            // splitLayout.Panel1
+            // 
+            this.splitLayout.Panel1.Controls.Add(this.titleLabel);
+            this.splitLayout.Panel1.Controls.Add(this.infoLabel);
+            this.splitLayout.Panel1.Controls.Add(this.stylesLabel);
+            this.splitLayout.Panel1.Controls.Add(this.styleCheckBoxList);
+            this.splitLayout.Panel1.Controls.Add(this.startQueryButton);
+            // 
+            // splitLayout.Panel2
+            // 
+            this.splitLayout.Panel2.Controls.Add(this.outputBox);
+            this.splitLayout.Size = new System.Drawing.Size(1051, 454);
+            this.splitLayout.SplitterDistance = 219;
+            this.splitLayout.TabIndex = 0;
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(20, 9);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(123, 17);
+            this.titleLabel.TabIndex = 4;
+            this.titleLabel.Text = "MusicDataMiner";
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.AutoSize = true;
+            this.infoLabel.Location = new System.Drawing.Point(20, 36);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(144, 102);
+            this.infoLabel.TabIndex = 3;
+            this.infoLabel.Text = "Browses through \r\npreprocessed freedb \r\n-information and \r\nmatches that with \r\nth" +
+                "e online \r\nMusicBrainz-data.";
+            // 
+            // stylesLabel
+            // 
+            this.stylesLabel.AutoSize = true;
+            this.stylesLabel.Location = new System.Drawing.Point(43, 148);
+            this.stylesLabel.Name = "stylesLabel";
+            this.stylesLabel.Size = new System.Drawing.Size(121, 17);
+            this.stylesLabel.TabIndex = 2;
+            this.stylesLabel.Text = "Styles to Process:";
+            // 
+            // styleCheckBoxList
+            // 
+            this.styleCheckBoxList.CheckOnClick = true;
+            this.styleCheckBoxList.FormattingEnabled = true;
+            this.styleCheckBoxList.Items.AddRange(new object[] {
+            "blues",
+            "classical",
+            "country",
+            "data",
+            "folk",
+            "jazz",
+            "misc",
+            "newage",
+            "reggae",
+            "rock",
+            "soundtrack"});
+            this.styleCheckBoxList.Location = new System.Drawing.Point(46, 168);
+            this.styleCheckBoxList.Name = "styleCheckBoxList";
+            this.styleCheckBoxList.Size = new System.Drawing.Size(120, 191);
+            this.styleCheckBoxList.Sorted = true;
+            this.styleCheckBoxList.TabIndex = 1;
+            // 
+            // startQueryButton
+            // 
+            this.startQueryButton.Location = new System.Drawing.Point(46, 382);
+            this.startQueryButton.Name = "startQueryButton";
+            this.startQueryButton.Size = new System.Drawing.Size(111, 23);
+            this.startQueryButton.TabIndex = 0;
+            this.startQueryButton.Text = "Start Queries";
+            this.startQueryButton.UseVisualStyleBackColor = true;
+            this.startQueryButton.Click += new System.EventHandler(this.startQueryButton_Click);
+            // 
+            // outputBox
+            // 
+            this.outputBox.BackColor = System.Drawing.SystemColors.Window;
+            this.outputBox.CausesValidation = false;
+            this.outputBox.Font = new System.Drawing.Font("Courier New", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputBox.Location = new System.Drawing.Point(0, 0);
+            this.outputBox.Multiline = true;
+            this.outputBox.Name = "outputBox";
+            this.outputBox.ReadOnly = true;
+            this.outputBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.outputBox.Size = new System.Drawing.Size(828, 454);
+            this.outputBox.TabIndex = 0;
+            this.outputBox.TextChanged += new System.EventHandler(this.outputBox_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1051, 454);
+            this.Controls.Add(this.splitLayout);
+            this.Name = "Form1";
+            this.Text = "Music Dataminer";
+            this.splitLayout.Panel1.ResumeLayout(false);
+            this.splitLayout.Panel1.PerformLayout();
+            this.splitLayout.Panel2.ResumeLayout(false);
+            this.splitLayout.Panel2.PerformLayout();
+            this.splitLayout.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
+
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitLayout;
+        private System.Windows.Forms.Label infoLabel;
+        private System.Windows.Forms.Label stylesLabel;
+        private System.Windows.Forms.Label titleLabel;
+
+        public System.Windows.Forms.TextBox outputBox;
+        public System.Windows.Forms.Button startQueryButton;
+        public System.Windows.Forms.CheckedListBox styleCheckBoxList;
     }
 }
 
--- MusicBrainzDotNet-2.1.1/examples/WindowsApplication1/MusicDBParser.cs
+++ MusicBrainzDotNet-2.1.1/examples/WindowsApplication1/MusicDBParser.cs
@@ -3,238 +3,281 @@ using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Forms;
 
 using CarlosAg.ExcelXmlWriter;
-
 using musicbrainz;
 
-class MusicDBParser
+namespace MusicDataminer
 {
-    public struct Country
+    class MusicDBParser
     {
-        public string acronym;
-        public string name;
-        public int gdpPerCapita;
-    };
+        private Form1 iForm;
 
-    public struct MusicBrainzAlbum
-    {
-        public Country country;
-        public int date;
-    };
-
-    public struct Album
-    {
-        public string title;
-
-        // probably store it as pointer :) ??
-        public string artist;
-        public string style;
-
-        public List<MusicBrainzAlbum> releases;
-    };
-
-
-    // private attributes
-    private List<Album> albums = new List<Album>();
-
-    private Hashtable countries = new Hashtable();
-
-
-    public bool GetMusicBrainzReleases(string artist, string albumName, MusicBrainz o,
-        List<MusicBrainzAlbum> releasesList, out string retrievedName)
-    {
-        retrievedName = albumName;
-        bool foundRelevantRelease = false;
-
-        Console.WriteLine("Searching for occurrences for: " + artist + " / " + albumName);
-        bool ret = o.Query(MusicBrainz.MBQ_FileInfoLookup, new String[] { "", artist, albumName, "", "", "" });
-
-        // Select the first album
-        o.Select(MusicBrainz.MBS_SelectLookupResult, 1);
-
-        string type;
-        o.GetResultData(MusicBrainz.MBE_LookupGetType, out type);
-        string fragment;
-        o.GetFragmentFromURL(type, out fragment);
-
-        // iterate through all the results
-        o.Select(MusicBrainz.MBS_Rewind);
-
-        if (!o.Select(MusicBrainz.MBS_SelectLookupResult, 1))
+        public struct Country
         {
-            return foundRelevantRelease;
+            public string acronym;
+            public string name;
+            public int gdpPerCapita;
+        };
+
+        public struct MusicBrainzAlbum
+        {
+            public Country country;
+            public int date;
+        };
+
+        public struct Album
+        {
+            public string title;
+
+            // probably store it as pointer :) ??
+            public string artist;
+            public string style;
+
+            public List<MusicBrainzAlbum> releases;
+        };
+
+        // private attributes
+        private List<Album> albums = new List<Album>();
+        private Hashtable countries = new Hashtable();
+
+        //Constructor
+        public MusicDBParser( Form1 aForm ) 
+        {
+            iForm = aForm;
+            this.ParseCountries("../../../countries_acronyms.txt");
         }
 
-        // NOTE: must be done before the next Select
-        int relevance = o.GetResultInt(MusicBrainz.MBE_LookupGetRelevance);
-
-        // if not sure about it, quit
-        if (relevance < 80)
+        public bool GetMusicBrainzReleases(string artist, string albumName, MusicBrainz o,
+            List<MusicBrainzAlbum> releasesList, out string retrievedName)
         {
-            return foundRelevantRelease;
-        }
+            retrievedName = albumName;
+            bool foundRelevantRelease = false;
 
-        // select the album
-        o.Select(MusicBrainz.MBS_SelectLookupResultAlbum);
 
-        o.GetResultData(MusicBrainz.MBE_AlbumGetAlbumName, out retrievedName);
+            
+            //Console.WriteLine("Searching for occurrences for: " + artist + " / " + albumName);
+            iForm.PrintLine("Searching for occurrences for: " + artist + " / " + albumName);
+            bool ret = o.Query(MusicBrainz.MBQ_FileInfoLookup, new String[] { "", artist, albumName, "", "", "" });
 
-        int nReleases = o.GetResultInt(MusicBrainz.MBE_AlbumGetNumReleaseDates);
+            // Select the first album
+            o.Select(MusicBrainz.MBS_SelectLookupResult, 1);
 
-        if (nReleases != 0)
-        {
-            albumName = retrievedName;
+            string type;
+            o.GetResultData(MusicBrainz.MBE_LookupGetType, out type);
+            string fragment;
+            o.GetFragmentFromURL(type, out fragment);
 
-            for (int i = 1; i <= nReleases; i++)
+            // iterate through all the results
+            o.Select(MusicBrainz.MBS_Rewind);
+
+            if (!o.Select(MusicBrainz.MBS_SelectLookupResult, 1))
             {
-                if (o.Select(MusicBrainz.MBS_SelectReleaseDate, i))
+                return foundRelevantRelease;
+            }
+
+            // NOTE: must be done before the next Select
+            int relevance = o.GetResultInt(MusicBrainz.MBE_LookupGetRelevance);
+
+            // if not sure about it, quit
+            if (relevance < 80)
+            {
+                return foundRelevantRelease;
+            }
+
+            // select the album
+            o.Select(MusicBrainz.MBS_SelectLookupResultAlbum);
+
+            o.GetResultData(MusicBrainz.MBE_AlbumGetAlbumName, out retrievedName);
+
+            int nReleases = o.GetResultInt(MusicBrainz.MBE_AlbumGetNumReleaseDates);
+
+            if (nReleases != 0)
+            {
+                albumName = retrievedName;
+
+                for (int i = 1; i <= nReleases; i++)
                 {
-                    foundRelevantRelease = true;
-
-                    string country;
-                    o.GetResultData(MusicBrainz.MBE_ReleaseGetCountry, out country);
-
-                    string date;
-                    o.GetResultData(MusicBrainz.MBE_ReleaseGetDate, out date);
-
-                    // add it to the list
-                    MusicBrainzAlbum release = new MusicBrainzAlbum();
-
-                    if (countries.ContainsKey(country.ToUpper()))
+                    if (o.Select(MusicBrainz.MBS_SelectReleaseDate, i))
                     {
-                        release.country = (Country)countries[country];
-                        release.date = int.Parse( date.Substring(0, 4) );
-                        releasesList.Add(release);
+                        foundRelevantRelease = true;
+
+                        string country;
+                        o.GetResultData(MusicBrainz.MBE_ReleaseGetCountry, out country);
+
+                        string date;
+                        o.GetResultData(MusicBrainz.MBE_ReleaseGetDate, out date);
+
+                        // add it to the list
+                        MusicBrainzAlbum release = new MusicBrainzAlbum();
+
+                        if (countries.ContainsKey(country.ToUpper()))
+                        {
+                            release.country = (Country)countries[country];
+                            release.date = int.Parse(date.Substring(0, 4));
+                            releasesList.Add(release);
+                        }
+
                     }
 
+                    o.Select(MusicBrainz.MBS_Back);
                 }
-
-                o.Select(MusicBrainz.MBS_Back);
             }
+
+            return foundRelevantRelease;
         }
 
-        return foundRelevantRelease;
-    }
-
-    private static void WriteSomeStuff()
-    {
-        Workbook book = new Workbook();
-        Worksheet sheet = book.Worksheets.Add("Sample");
-        WorksheetRow row = sheet.Table.Rows.Add();
-
-        // Header
-        row.Cells.Add("Artist");
-        row.Cells.Add("Album/Release");
-        row.Cells.Add("Similar Artist");
-        row.Cells.Add("Date");
-        row.Cells.Add("Country");
-
-        // Rows
-        row = sheet.Table.Rows.Add();
-        row.Cells.Add("Pink Floyd");
-        row.Cells.Add("The Division Bell");
-        row.Cells.Add("Someone :)");
-        row.Cells.Add("2000-10-05");
-        row.Cells.Add("SE");
-
-        book.Save(@"../../../test.xls");
-
-        //string fileName = "test.txt";  // a sample file name
-
-        //// Delete the file if it exists.
-        //if (File.Exists(fileName))
-        //{
-        //    File.Delete(fileName);
-        //}
-
-        //// Create the file.
-        //FileStream fs = File.Create(fileName, 1024);
-
-        //// Add some information to the file.
-        //byte[] info = new System.Text.UTF8Encoding(true).GetBytes("This is some text in the file.");
-        //fs.Write(info, 0, info.Length);
-
-        //// Open the file and read it back.
-        //StreamReader sr = File.OpenText(fileName);
-        //string s = "";
-        //while ((s = sr.ReadLine()) != null) 
-        //{
-        //    System.Console.WriteLine(s);
-        //}
-    }
-
-    public void ParseCountries(string filename)
-    {
-        // Open the file and read it back.
-        StreamReader sr = File.OpenText(filename);
-        string text = "";
-        while ((text = sr.ReadLine()) != null)
+        private static void WriteSomeStuff()
         {
-            char[] delimiterChars = { '\t' };
+            Workbook book = new Workbook();
+            Worksheet sheet = book.Worksheets.Add("Sample");
+            WorksheetRow row = sheet.Table.Rows.Add();
 
-            string[] words = text.Split(delimiterChars);
+            // Header
+            row.Cells.Add("Artist");
+            row.Cells.Add("Album/Release");
+            row.Cells.Add("Similar Artist");
+            row.Cells.Add("Date");
+            row.Cells.Add("Country");
 
-            if (words.Length > 2)
-            {
-                Country country;
-                country.acronym = words[0].ToUpper();
-                country.name = words[1];
-                country.gdpPerCapita = int.Parse(words[2]);
+            // Rows
+            row = sheet.Table.Rows.Add();
+            row.Cells.Add("Pink Floyd");
+            row.Cells.Add("The Division Bell");
+            row.Cells.Add("Someone :)");
+            row.Cells.Add("2000-10-05");
+            row.Cells.Add("SE");
 
-                if( ! countries.ContainsKey( country.acronym ) )
-                {
-                    countries.Add(country.acronym, country);
-                }
-            }
+            book.Save(@"../../../test.xls");
+
+            //string fileName = "test.txt";  // a sample file name
+
+            //// Delete the file if it exists.
+            //if (File.Exists(fileName))
+            //{
+            //    File.Delete(fileName);
+            //}
+
+            //// Create the file.
+            //FileStream fs = File.Create(fileName, 1024);
+
+            //// Add some information to the file.
+            //byte[] info = new System.Text.UTF8Encoding(true).GetBytes("This is some text in the file.");
+            //fs.Write(info, 0, info.Length);
+
+            //// Open the file and read it back.
+            //StreamReader sr = File.OpenText(fileName);
+            //string s = "";
+            //while ((s = sr.ReadLine()) != null) 
+            //{
+            //    System.Console.WriteLine(s);
+            //}
         }
-    }
 
-    public void ParseMusicStyle(string style, MusicBrainz queryObject)
-    {
-        // Open the file and read it back.
-        StreamReader sr = File.OpenText("../../../" + style + ".txt");
-        string text = "";
-        while ((text = sr.ReadLine()) != null)
+        public void ParseCountries(string filename)
         {
-            char[] delimiterChars = { '\t' };
-
-            string[] tokens = text.Split(delimiterChars);
-
-            Album album = new Album();
-
-            // Lowercase all the tokens
-            album.artist = tokens[0].ToLower();
-            album.title = tokens[1].ToLower();
-
-            if (tokens.Length < 3 || tokens[2].Trim().Length == 0)
+            // Open the file and read it back.
+            StreamReader sr = File.OpenText(filename);
+            string text = "";
+            while ((text = sr.ReadLine()) != null)
             {
-                album.style = style;
-            }
-            else
-            {
-                album.style = tokens[2].ToLower();
-            }
+                char[] delimiterChars = { '\t' };
 
-            // search for info in the MusicBrainz DB
-            List<MusicBrainzAlbum> releases = new List<MusicBrainzAlbum>();
-            string retrievedName;
-            bool foundSomething = GetMusicBrainzReleases(album.artist, album.title, queryObject, 
-                releases, out retrievedName);
-           
-            if (foundSomething)
-            {
-                album.title = retrievedName;
-                album.releases = releases;
-                albums.Add(album);
+                string[] words = text.Split(delimiterChars);
 
-                Console.WriteLine("Added Album: " + album.title + " Artist: " + album.artist + ": ");
-                foreach (MusicBrainzAlbum release in album.releases)
+                if (words.Length > 2)
                 {
-                    Console.WriteLine("\tCountry: " + release.country.name + "  Date: " + release.date);
+                    Country country;
+                    country.acronym = words[0].ToUpper();
+                    country.name = words[1];
+                    country.gdpPerCapita = int.Parse(words[2]);
+
+                    if (!countries.ContainsKey(country.acronym))
+                    {
+                        countries.Add(country.acronym, country);
+                    }
                 }
             }
         }
-    }
 
+        ParseMusicStyleDelegate iParseMusicStyleDelegate;
+
+        private delegate void ParseMusicStyleDelegate(string style, MusicBrainz queryObject);
+        private void ParseMusicStyleCallback(IAsyncResult ar)
+        {
+            Console.WriteLine("DONE");
+        }
+
+        public void AsyncParseByStyle(string style, MusicBrainz queryObject) 
+        {
+            iParseMusicStyleDelegate = new ParseMusicStyleDelegate( ParseMusicStyle );
+            AsyncCallback callback = new AsyncCallback( ParseMusicStyleCallback );
+            iParseMusicStyleDelegate.BeginInvoke( style, queryObject, callback, 123456789 );
+        }
+
+        public void ParseMusicStyle(string style, MusicBrainz queryObject)
+        {
+            // Open the file and read it back.
+            StreamReader sr = File.OpenText("../../../" + style + ".txt");
+            string text = "";
+            
+            int lineNumber = 0;
+            while ((text = sr.ReadLine()) != null && iForm.queryOnGoing )
+            {
+                char[] delimiterChars = { '\t' };
+
+                string[] tokens = text.Split(delimiterChars);
+
+                Album album = new Album();
+
+                // Lowercase all the tokens
+                album.artist = tokens[0].ToLower();
+                album.title = tokens[1].ToLower();
+
+                if (tokens.Length < 3 || tokens[2].Trim().Length == 0)
+                {
+                    album.style = style;
+                }
+                else
+                {
+                    album.style = tokens[2].ToLower();
+                }
+
+                // search for info in the MusicBrainz DB
+                List<MusicBrainzAlbum> releases = new List<MusicBrainzAlbum>();
+                string retrievedName;
+                bool foundSomething = GetMusicBrainzReleases(album.artist, album.title, queryObject,
+                    releases, out retrievedName);
+
+                if (foundSomething)
+                {
+                    album.title = retrievedName;
+                    album.releases = releases;
+                    albums.Add(album);
+
+                    //Console.WriteLine("Added Album: " + album.title + " Artist: " + album.artist + ": ");
+                    iForm.PrintLine("Added Album: " + album.title + " Artist: " + album.artist + ": ");
+                    foreach (MusicBrainzAlbum release in album.releases)
+                    {
+                        //Console.WriteLine("\tCountry: " + release.country.name + "  Date: " + release.date);
+                        iForm.PrintLine("\tCountry: " + release.country.name + "  Date: " + release.date);
+                    }
+                }
+                lineNumber++;
+            }
+
+            //query was interrupted
+            if (!iForm.queryOnGoing)
+            {
+                iForm.PrintLine("Stopped at line: " + lineNumber);
+            }
+            //end ok
+            else 
+            {
+                iForm.PrintLine("Queried lines: " + lineNumber);
+            }
+        }
+
+    }
 }
